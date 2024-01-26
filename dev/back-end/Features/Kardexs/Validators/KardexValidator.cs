295fbe2b82255293315d8792e273e9c5ebad1d20
Mateus Quintino <mateus.quintino@valorup.com.br>
2024-01-26 15:31:27 -0300

    Validações da entidade Kardex

--- dev/back-end/Features/Kardexs/Validators/KardexValidator.cs
+++ dev/back-end/Features/Kardexs/Validators/KardexValidator.cs
@@ -6,8 +6,17 @@ namespace ConvenienceStore.Features.Kardexs.Validators
 {
     public class KardexValidator : AbstractValidator<Kardex>
     {
-        public KardexValidator() 
+        public KardexValidator()
         {
+            RuleFor(kardex => kardex.ProductId)
+            .NotEmpty()
+            .WithMessage("O Id do Produto deve ser preenchido.");
+
+            RuleFor(kardex => kardex.Quantity)
+            .NotEmpty()
+            .GreaterThan(0)
+            .WithMessage("A quantidade deve ser informada e maior do que zero.");
+
         }
     }
 }
