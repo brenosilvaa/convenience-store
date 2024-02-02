129dc4040327ff651dd5e42e6201d2daf339fd58
Marcos Felipe <98355126+marcosfelipee18@users.noreply.github.com>
2024-02-02 15:43:16 -0300

    Ajustes na entidade de Product

---
+++ dev/back-end/Features/Products/Configs/ProductConfig.cs
@@ -0,0 +1,18 @@
+﻿using ConvenienceStore.Features.Products.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ConvenienceStore.Features.Products.Configs
+{
+    public class ProductConfig : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.HasOne(x => x.User)
+                    .WithMany(x => x.Products)
+                      .HasForeignKey(x => x.UserId);
+        }
+    }
+}
