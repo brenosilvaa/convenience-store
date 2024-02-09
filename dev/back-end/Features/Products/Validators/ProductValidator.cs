18008dbbc261841e9b3ecd06c8adb1673b92523b
Marcos Felipe <98355126+marcosfelipee18@users.noreply.github.com>
2024-02-09 15:11:57 -0300

    Criado validators, vms e repositório de Products

---
+++ dev/back-end/Features/Products/Validators/ProductValidator.cs
@@ -0,0 +1,24 @@
+﻿using ConvenienceStore.Features.Products.Models;
+using FluentValidation;
+
+namespace ConvenienceStore.Features.Products.Validators
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(product => product.Name)
+                .NotEmpty()
+                .MinimumLength(3)
+                .WithMessage("O campo Nome deve conter ao menos 3 caracteres.");
+
+            RuleFor(product => product.Value)
+                .NotEmpty()
+                .WithMessage("O campo Valor deve ser maior que 0.");
+
+            RuleFor(product => product.UserId)
+                .NotEmpty()
+                .WithMessage("O campo Usuário é obrigatório.");
+        }
+    }
+}
---
+++ dev/back-end/Features/Products/ViewModels/ProductVM.cs
@@ -0,0 +1,29 @@
+﻿using ConvenienceStore.Features.Pixes.ValueObjects;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConvenienceStore.Features.Products.ViewModels
+{
+    public class ProductVM
+    {
+        [Required] public Guid Id { get; set; }
+        [Required] public required string Name { get; set; }
+        public string? Image { get; set; }
+        public string? Description { get; set; }
+        [Required] public required decimal Value { get; set; }
+        [Required] public required Guid UserId { get; set; }
+
+        public ProductVM()
+        {
+        }
+
+        public ProductVM(Guid id, string name, string? image, string? description, decimal value, Guid userId)
+        {
+            Id = id;
+            Name = name;
+            Image = image;
+            Description = description;
+            Value = value;
+            UserId = userId;
+        }
+    }
+}
