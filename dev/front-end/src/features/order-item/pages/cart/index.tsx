5612f551472355fc5a6e5c90a35d14d9ebd0ba99
lucasbalani <lucas.balani@valorup.com.br>
2024-04-03 22:28:06 -0300

    "Add remove order item functionality to cart page"
    This commit adds the ability to remove an order item from the cart page. It includes the following changes:
    
    * Importing `Button`, `Divider`, and `useSetRecoilState` from `@mui/material` and `recoil`.
    * Adding a `removeOrderItemFromCart` function that removes an order item from the cart state and displays a success snackbar.
    * Updating the `ListItemText` primary component to include a "Remover" link that triggers the `removeOrderItemFromCart` function when clicked.
    * Adding a `Divider` component to separate the product name from the "Remover" link.
    
    Note: The commit message assumes that the code diff provided is accurate and complete.

--- dev/front-end/src/features/order-item/pages/cart/index.tsx
+++ dev/front-end/src/features/order-item/pages/cart/index.tsx
@@ -1,20 +1,49 @@
-import { List, ListItem, ListItemAvatar, Avatar, ListItemText, Box, Typography } from "@mui/material";
-import { useRecoilValue } from "recoil";
+import { List, ListItem, ListItemAvatar, Avatar, ListItemText, Box, Typography, Button, Divider } from "@mui/material";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { cartState } from "../../states/cart-state";
 import { CreateOrderItem } from "../../models/create-order-item";
+import { IoMdRemoveCircle } from "react-icons/io";
+import { enqueueSnackbar, useSnackbar } from "notistack";
 
 const CartPage = () => {
     const cart = useRecoilValue(cartState);
+    const setCart = useSetRecoilState(cartState);
+    const { enqueueSnackbar } = useSnackbar();
+
+    const removeOrderItemFromCart = (index: number) => {
+        let cartList: CreateOrderItem[] = [...cart];
+
+        const item = cartList[index];
+
+        cartList.splice(index, 1);
+
+        setCart(cartList);
+
+        enqueueSnackbar(`${item.product?.name} removido com sucesso`, { variant: 'success', autoHideDuration: 3000,  });
+    }
 
     return (
         <List sx={{ width: 1, bgcolor: 'background.paper' }}>
-            {cart.map((item: CreateOrderItem) => (
+            {cart.map((item: CreateOrderItem, index: number) => (
                 <ListItem key={item.productId} sx={{ marginBottom: 5, border: "1px solid #ccc" }}>
                     <ListItemAvatar>
                         <Avatar alt={item.product?.name} src={item.product?.image} />
                     </ListItemAvatar>
                     <ListItemText
-                        primary={item.product?.name}
+                        primary={
+                            <>
+                                {item.product?.name}
+                                <Box sx={{
+                                    marginTop: "-5px",
+                                    cursor: "pointer"
+                                }}>
+                                    <Typography onClick={() => removeOrderItemFromCart(index)} component={"span"} variant="caption" color={"red"}>
+                                        <IoMdRemoveCircle style={{ marginRight: 2 }} />Remover
+                                    </Typography>
+                                </Box>
+                                <Divider sx={{ marginBottom: 2 }} />
+                            </>
+                        }
                         secondary={
                             <Box>
                                 <Box sx={{ direction: "flex", justifyContent: "space-between" }}>
